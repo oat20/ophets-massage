a81baaec8c54086f92a70f50327c72e3e1c67e45
oat20 <iceeagle99@gmail.com>
2022-02-23 16:04:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
+<title>OPHETS</title>
 <!-- include file CSS -->
 <link rel="stylesheet" href="jquery-ui-1.9.1.custom/css/redmond/jquery-ui-1.9.1.custom.css" />
 <link rel="stylesheet" href="css/style.css" />
